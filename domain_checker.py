30cf4406d4c391f3fdc5ef79bb0dd75b7ff3741a
Sohaib <sohaibanwaar36@gmail.com>
2024-08-10 16:00:55 +0500

    add: adding prompt fixies

---
+++ domain_checker.py
@@ -0,0 +1,12 @@
+"""This file will be responsible for checking the name availability on the Godaddy
+
+Inputs: Array of Names
+functionality: Send the names to goDaddy.com one by one and see the status of names given
+Output: Store status in excel file
+
+
+Function defination:
+
+def name_checker(names: list):
+    pass
+"""
---
+++ namefinder.py
@@ -0,0 +1,15 @@
+"""This file will be responsible to get new names from OpenAI
+
+Inputs: Prompt
+functionality:
+    1. send the prompt to openai get the names
+    2. Split names into list of names got by the openAI
+Output: List of Names
+
+
+Function defination:
+
+def name_finder(prompt: str) -> list:
+    pass
+    return  names
+"""
